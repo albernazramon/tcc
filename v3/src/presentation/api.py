d083f88871e91cfdb6be25f380791cdf05afd331
Ramon Albernaz dos Santos <ramonalber.ra@gmail.com>
2026-03-07 19:42:21 -0300

    tentando corrigir o rag

--- v3/src/presentation/api.py
+++ v3/src/presentation/api.py
@@ -30,7 +30,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DB_PATH = os.path.join(BASE_DIR, "RAG", "chroma_db")
 
 retriever = ChromaVectorRetriever(db_path=DB_PATH)
